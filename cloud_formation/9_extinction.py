a6610762e9070b5791bb5c81730785c06bcade12
quntai <roska.posti.turha@gmail.com>
2017-01-04 21:56:35 +0200

    9 extinction by function of particle size

---
+++ cloud_formation/9_extinction.py
@@ -0,0 +1,38 @@
+# Mika "AgenttiX" Mäki & Alpi Tolvanen, 2017
+# Strongly based on:
+# Tampere University of Technology course FYS-1320 ESIMERKKI_EKSTINKTIOTEHOKKUUS.m by Joni Kalliokoski 2014-09-09
+
+import toolbox
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+This code calculates extinction (absorption) of cloud gas by function of water droplet particle size
+
+"""
+
+N = 10000*10^5      # assuming number concentration of 10000/cm^3 --> 10^9/m^3
+L = 1               # length of a pipe, assuming "about a meter" is a meter (Opintomoniste)
+
+
+m = 1.33 + 0.001j
+lambda_var = 635
+dp = np.logspace(1,4,1000)
+
+Qext = np.zeros(dp.size)
+
+for k in range(dp.size):
+    result = toolbox.mie(m, np.pi*dp[k] / lambda_var)
+    Qext[k] = result[3]
+
+#plt.plot(dp, Qext)
+#plt.xscale("log")
+#plt.yscale("log")
+#plt.show()
+
+
+sigma_ext=(np.pi*N*(dp*1e-9)**2*Qext)/4;   # dp in nanometers
+ext = 1- np.exp(-sigma_ext*L);
+
+plt.plot(dp, ext)
+plt.show()
---
+++ cloud_formation/kohta_9_ekstinktio.m
@@ -0,0 +1,41 @@
+%9) ekstinkiton eli vaimenemisen laskeminen hiukkaskoon funktiona
+
+N = 10000*10^5     % oletetaan että pitoisuus 10000/cm^3 -->10^9/m^3
+L = 1         % oletetaan että "noin metrin pituinen putki" on metrin pitkä
+
+
+% Käytetään pohjana ESIMERKKI_EKSTINKTIOTEHOKKUUS.m
+
+% Esimerkkitiedosto ekstinktiotehokkuuksien laskentaan
+% Joni Kalliokoski, 9.9.2014
+
+m=1.33+0.001i; % veden taitekerroin
+lambda=635; % valon aallonpituus (nm)
+dp = logspace(1,4,1000); % hiukkaskokovektori, joissa ekstintiotehokuudet
+                         % lasketaan (nm)
+
+% Lasketaan Mie-parametrit:
+Qext = zeros(size(dp)); %Alustetaan ekstintiovektori
+for k = 1:numel(dp)
+    result = Mie(m, pi*dp(k)/lambda); %Mie-parametrien laskenta
+    Qext(k) = result(4); %Funktio palauttaa vektorin, jonka 4. alkio
+    % on haluttu ekstinktiotehokkuus, muita arvoja ei tässä työssä tarvita.
+end
+
+sigma_ext=(pi*N.*(dp.*1e-9).^2.*Qext)/4;   %huomaa nanometrit
+
+ext = 1- exp(-sigma_ext.*L);
+
+
+%Piirretään ekstintiotehokkuus hiukkaskoon funktiona
+figure(2)
+loglog(dp,Qext,'-k')
+xlabel('d_p (nm)')
+ylabel('Q_{ext}')
+
+%Piirretään ekstinktio hiukkaskoon funktiona
+figure(1)
+plot(dp,ext,'-k')
+xlabel('d_p (nm)')
+ylabel('ekstinktio')
+
