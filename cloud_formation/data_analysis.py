e1cbfda4b673e5f80820e1d4dd37a8f329ec2a4e
quntai <roska.posti.turha@gmail.com>
2017-02-04 12:33:46 +0200

    Minor tweaking Add new module toolbox_2, flip extinction data upside down, enchance "find_drop_index()" algorithm, change x-axis to time Also new feature in progress "simulate_extinction()"

--- cloud_formation/data_analysis.py
+++ cloud_formation/data_analysis.py
@@ -21,14 +21,14 @@
 # npTDMS is a library for reading TDMS files created by LabVIEW
 # https://pypi.python.org/pypi/npTDMS/
 import nptdms
-
+import toolbox_2
 # PyQtGraph can be found from Ubuntu repositories as python3-pyqtgraph
 # http://www.pyqtgraph.org/
 import pyqtgraph as pg
 import numpy as np
 
 from pyqtgraph.Qt import QtGui
-from math import factorial
+
 
 
 class Measurement:
@@ -86,15 +86,19 @@ class Main:
             layout.addWidget(label, i, 0)
 
         self.__input_meas = pg.SpinBox(value=self.meas_selected_number, int=True, minStep=1, step=1)
+        self.__input_meas.editingFinished.connect(self.update_change)
         layout.addWidget(self.__input_meas, 0, 1)
 
         self.__input_series = pg.SpinBox(value=self.meas_selected_series, int=True, minStep=1, step=1)
+        self.__input_series.editingFinished.connect(self.update_change)
         layout.addWidget(self.__input_series, 1, 1)
 
         self.__input_select = pg.SpinBox(value=self.selected_data, int=True, minStep=1, step=1, min=1)
+        self.__input_select.editingFinished.connect(self.update_change)
         layout.addWidget(self.__input_select, 2, 1)
 
         self.__input_noice = pg.SpinBox(value=self.noice_reduction_number, int=True, minStep=1, step=1, min=0)
+        self.__input_noice.editingFinished.connect(self.update_change)
         layout.addWidget(self.__input_noice, 3, 1)
 
         self.__updateButton = QtGui.QPushButton("Update")
@@ -142,27 +146,27 @@ class Main:
             raise ValueError
 
         if self.selected_data == 1:
-            data = self.remove_noice(measurement.p_diff)
+            data = toolbox_2.remove_noice(measurement.p_diff, self.noice_reduction_number)
         elif self.selected_data == 2:
-            data = self.remove_noice(measurement.p_abs)
+            data = toolbox_2.remove_noice(measurement.p_abs, self.noice_reduction_number)
         elif self.selected_data == 3:
-            data = self.remove_noice(measurement.ext)
+            data = toolbox_2.remove_noice(toolbox_2.flip_and_normalize(measurement.ext), self.noice_reduction_number)
 
         else:
             raise ValueError
 
         if self.first_update:
-            self.curve_select = self.plot_select.plot(data)
-            self.curve_zoom = self.plot_zoom.plot(data)
+            self.curve_select = self.plot_select.plot(measurement.time, data)
+            self.curve_zoom = self.plot_zoom.plot(measurement.time, data)
             self.first_update = False
         else:
-            self.curve_select.setData(data)
-            self.curve_zoom.setData(data)
+            self.curve_select.setData(measurement.time, data)
+            self.curve_zoom.setData(measurement.time, data)
 
         # sets the graphs for the drop-point
-        index_of_drop = self.find_drop_index(measurement.p_diff)
-        self.plot_select.setXRange(index_of_drop - 10000, index_of_drop + 10000, padding=0)
-        self.plot_zoom.setXRange(index_of_drop - 1000, index_of_drop + 3000, padding=0)
+        index_of_drop = toolbox_2.find_drop_index(measurement.p_diff)
+        self.plot_select.setXRange(measurement.time[index_of_drop - 10000], measurement.time[index_of_drop + 10000], padding=0)
+        self.plot_zoom.setXRange(measurement.time[index_of_drop - 1000], measurement.time[index_of_drop + 3000], padding=0)
 
         self.update_zoom_region()
         #self.update_zoom_plot()
@@ -179,96 +183,9 @@ class Main:
             measurements.append(Measurement(folder + str(i) + ".tdms"))
         return measurements
 
-    # From http://scipy.github.io/old-wiki/pages/Cookbook/SavitzkyGolay
-    def savitzky_golay(self, y, window_size, order, deriv=0, rate=1):
-        r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-        The Savitzky-Golay filter removes high frequency noise from data.
-        It has the advantage of preserving the original shape and
-        features of the signal better than other types of filtering
-        approaches, such as moving averages techniques.
-        Parameters
-        ----------
-        y : array_like, shape (N,)
-            the values of the time history of the signal.
-        window_size : int
-            the length of the window. Must be an odd integer number.
-        order : int
-            the order of the polynomial used in the filtering.
-            Must be less then `window_size` - 1.
-        deriv: int
-            the order of the derivative to compute (default = 0 means only smoothing)
-        Returns
-        -------
-        ys : ndarray, shape (N)
-            the smoothed signal (or it's n-th derivative).
-        Notes
-        -----
-        The Savitzky-Golay is a type of low-pass filter, particularly
-        suited for smoothing noisy data. The main idea behind this
-        approach is to make for each point a least-square fit with a
-        polynomial of high order over a odd-sized window centered at
-        the point.
 
-        References
-        ----------
-        .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-           Data by Simplified Least Squares Procedures. Analytical
-           Chemistry, 1964, 36 (8), pp 1627-1639.
-        .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-           W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-           Cambridge University Press ISBN-13: 9780521880688
-        """
 
-        try:
-            window_size = np.abs(np.int(window_size))
-            order = np.abs(np.int(order))
-        except ValueError as msg:
-            raise ValueError("window_size and order have to be of type int")
-        if window_size % 2 != 1 or window_size < 1:
-            raise TypeError("window_size size must be a positive odd number")
-        if window_size < order + 2:
-            raise TypeError("window_size is too small for the polynomials order")
-        order_range = range(order + 1)
-        half_window = (window_size - 1) // 2
-        # precompute coefficients
-        b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
-        m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
-        # pad the signal at the extremes with
-        # values taken from the signal itself
-        firstvals = y[0] - np.abs(y[1:half_window + 1][::-1] - y[0])
-        lastvals = y[-1] + np.abs(y[-half_window - 1:-1][::-1] - y[-1])
-        y = np.concatenate((firstvals, y, lastvals))
-        return np.convolve(m[::-1], y, mode='valid')
 
-    def remove_noice(self, data):
-        if(self.noice_reduction_number == 0):
-            return data
-        if(self.noice_reduction_number < 0):
-            raise ValueError("Noice reduction can't be negative")
-        else:
-            return self.savitzky_golay(data, (self.noice_reduction_number*50 + 1), 3) # window size 251, polynomial order 3
-
-    def find_drop_index(self, data):
-        # This function will find the index of biggest drop on data vector. Intended for data=measurement.p_diff.
-        # Finds the biggest difference drop, or in other words the most negative coarse derivative.
-        index_skip = 200        # every single index is not tested for performance reasons
-        index_spacing = 500    # the time interval between which difference is tested
-        border_margin = np.int(index_spacing / index_skip) + 1 # for cropping off the ends of vector
-
-        greatest_difference = -1
-        index_of_greatest = -1
-
-        for i in range(border_margin, (np.int(data.size/index_skip)-border_margin)):
-            index = i * index_skip
-
-            # find the difference of data around the index.
-            difference = data[index - index_spacing] - data[index + index_spacing]
-
-            if (difference > greatest_difference):
-                greatest_difference = difference
-                index_of_greatest = index
-
-        return index_of_greatest
 
 
 def main():
---
+++ cloud_formation/toolbox_2.py
@@ -0,0 +1,183 @@
+# Mika "AgenttiX" Mäki & Alpi Tolvanen, 2017
+
+# Toolbox_2 was created to move functions out from data_analysis.py
+
+
+from math import factorial
+import numpy as np
+
+# From http://scipy.github.io/old-wiki/pages/Cookbook/SavitzkyGolay
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order + 1)
+    half_window = (window_size - 1) // 2
+    # precompute coefficients
+    b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
+    m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs(y[1:half_window + 1][::-1] - y[0])
+    lastvals = y[-1] + np.abs(y[-half_window - 1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve(m[::-1], y, mode='valid')
+
+def remove_noice(data, noice_reduction_number):
+    if(noice_reduction_number == 0):
+        return data
+    if(noice_reduction_number < 0):
+        raise ValueError("Noice reduction can't be negative")
+    else:
+        return savitzky_golay(data, (noice_reduction_number*50 + 1), 3) # window size 251, polynomial order 3
+
+def flip_and_normalize(data):
+    index = find_drop_index(data)
+    zero_level = np.mean(data[index-10000:index-6000])
+    return 1-data/zero_level
+
+def find_drop_index(data):
+    """
+    Finds the index of beginning of the largest decline in vector. Intended for data=measurement.p_diff, in which the biggest
+    difference drop indicates the moment of pressure release.
+    :param data: type of array, works best for measurement.p_diff
+    :return: index of the beginning of the pressure drop
+    """
+    # This funtion finds the coarse index of biggest drop first, and then it fine tunes the index to the beginning of drop.
+
+    # Finding the biggest decline with coarse method, this is done by iterating differences on some interval.
+    index_skip = 200        # every single index is not tested for performance reasons,
+    index_spacing = 500     # the time interval between which difference is tested
+    border_margin = np.int(index_spacing / index_skip) + 1 # for cropping off the ends of an array to avoid out_of_range
+    greatest_difference = -1
+    index_of_greatest_diff = -1
+    i_of_greatest = -1      # i is sparce index, meaning for every i, there's {index_skip=200} normal indexes
+
+    for i in range(border_margin, (np.int(data.size/index_skip)-border_margin)):
+        index = i * index_skip
+        # finds the difference of data around the index.
+        difference = data[index - index_spacing] - data[index + index_spacing]
+        if (difference > greatest_difference):
+            greatest_difference = difference
+            index_of_greatest_diff = index
+
+
+    # Fine tuning of index to the beginning of decline:
+
+    # Minium and maxium of data before the drop occurs. Deviation is caused by random noice.
+    minium_before_drop = np.amin(data[index_of_greatest_diff-4000 : index_of_greatest_diff-2000])
+    maxium_before_drop = np.amax(data[index_of_greatest_diff-4000 : index_of_greatest_diff-2000])
+    threshold = minium_before_drop - (maxium_before_drop-minium_before_drop)
+
+    fine_tuned_index_of_drop = -1
+    for i in range(index_of_greatest_diff-2000, index_of_greatest_diff+1000):
+        if (data[i] < threshold):
+            fine_tuned_index_of_drop = i
+    if (fine_tuned_index_of_drop == -1):
+        raise "Something went wrong in fine-tuning the index of pressure drop!"
+    fine_tuned_index_of_drop -= 150     # -150 is approximation that sets index somewhat just before the drop starts
+
+    return fine_tuned_index_of_drop
+
+def get_pressure_change(data):
+    """
+    Returns initial and final pressures.
+
+    :param data: must be instance of class Measurement
+    :return: initial_pressure, final_pressure
+    """
+    drop = find_drop_index(data.p_diff)
+
+    final_pressure = np.mean(data.p_abs[drop+6000,drop+16000])
+    drop_heigth = np.mean(data.p_diff[drop-6000 : drop-4000]) - np.mean(data.p_diff[drop+4000 : drop+6000])
+
+    initial_pressure = final_pressure + drop_heigth
+
+    return initial_pressure, final_pressure
+
+def simulate_extinction(particle_size, p_i, p_f, particle_dens):
+    """
+    Simulates particle growth extinction with given parameters. Growing particles are all the same size.
+    :param particle_size:
+    :param p_i: initial pressure
+    :param p_f: final pressure
+    :param n_extinction: the number of particles in unit volume  (#/m^3)
+    :return:
+    """
+    import toolbox
+
+    # Constants
+    temp_i = 296.15  # (K), 23 deg C, from instructions
+    diff = 0.282e-4  # Diffusion coefficient for water (m^2/s)
+    gas_const = 8.3144621  # (Pa*m^3)/(mol*K), from example code
+    surface_tension = 72.8e-3  # (N/m), From example code                      FIXED
+    m_mol = 18.016e-3  # Molar mass of water (kg/mol)
+    rho_wat = 998.20  # Density of water (kg/m^3)
+    evap_E = 2260e3  # Evaporation energy of water (J/kg)
+    thermal_con_air = 0.0257  # Thermal conductivity of air (W/(m*K))
+    heat_capacity_ratio = 1.4  # For (dry) air, from https://en.wikipedia.org/wiki/Heat_capacity_ratio
+    m = 1.33 + 0.001  # Refractive index of water
+    water_a = 10.23
+    water_b = 1750
+    water_c = 38
+    wavelength = 635  # Wavelength of our laser
+    length = 1  # (m)
+    tmax = 4  # The maximum time up which to compute to (s)
+    #particle_dens = 1e4 * 1e6  # particle density (#/m^3)
+
+    partial_pressure = (p_f/p_i) * toolbox.water_pvap(temp_i)   # Partial water pressure after adiabatic expansion
+    temp_f = toolbox.final_temp(temp_i, p_f, p_i, heat_capacity_ratio)  # Temperature after adiabatic expansion
+
+    t, dp, pw = toolbox.solve_growth(temp_f, diff, m_mol, evap_E, thermal_con_air, rho_wat, surface_tension,
+                                           particle_dens, tmax, particle_size, partial_pressure)
+    q_ext = toolbox.q_ext(dp, m, wavelength)
+    sigma_ext = toolbox.extinction_factor(particle_dens, dp, q_ext)
+    ext = toolbox.extinction(sigma_ext, length)
+
+    smallest_growing_particle =  toolbox.minimum_particle_diameter_2(p_i, p_f, temp_f, heat_capacity_ratio,
+                                           water_a, water_b, water_c, m_mol, surface_tension, rho_wat)
+    return ext, smallest_growing_particle
